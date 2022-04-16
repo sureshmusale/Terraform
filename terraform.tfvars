baf75c329c5ffd9858648281c97daf2afaa582a8
Suresh Musale <sureshbe405@gmail.com>
2022-04-16 09:29:27 +0200

    bucketv1.0

---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+bucket_name = "terraform-bucket-learning"
+storage_class = "REGIONAL"
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "bucket_name" {
+  type = string
+  description = "bucket name"
+}
+variable "bucket_location" {
+  type = string
+  default = "us-east1"
+}
+variable "project_id" {
+  type=string
+  default = "terraformproject-347311"
+}
+variable "storage_class" {
+   type = string
+}
